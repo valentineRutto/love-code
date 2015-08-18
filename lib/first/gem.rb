ab0f7017e7ba4d0d6035c81cd41c58675dc6a838
valentinerutto <rutto28@gmail.com>
2015-08-18 13:10:35 +0300

    initial commit

---
+++ lib/first/gem.rb
@@ -0,0 +1,10 @@
+require "first/gem/version"
+
+module First
+  module Gem
+    # Your code goes here...
+ def self.hi
+    puts "Hello world!"
+  end
+  end
+end
---
+++ lib/first/gem.rb~
@@ -0,0 +1,7 @@
+require "first/gem/version"
+
+module First
+  module Gem
+    # Your code goes here...
+  end
+end
